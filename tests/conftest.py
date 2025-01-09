7d8a3e620ae7aec7a8349ebcb2afb31028e6e9de
jcsellers <justin.sellers@gmail.com>
2025-01-09 16:57:07 -0500

    added src

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,12 +5,8 @@ import sys
 print("PYTHONPATH:", sys.path)
 print("Modules:", sys.modules.keys())
 
-scripts_path = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "../scripts")
-)
-sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src"))
-)
+scripts_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../scripts"))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 
 modules = [module.name for module in pkgutil.iter_modules(["src"])]
 print("Modules in src:", modules)
